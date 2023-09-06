c1f69a8184d699531a7b01ee295b23b535ffad3c
MajorRock06 <mikk476r@mail.hansenberg.dk>
2023-09-06 11:20:02 +0200

    god start
    sat gode ting op

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿Page A = new Page(MyNotes.VariablesPageOne);
+﻿//kapitel 1
+Page page1 = new Page(MyNotes.VariablesPageOne);
+
+List<Page> C1pages = new List<Page>();
+C1pages.Add(page1);
+
+
+//kapitel 2
+List<Page> C2pages = new List<Page>();
 
 
 
-Chapter chapterOne = new Chapter ("variables", Page.A);
+
+//kapitler
+Chapter chapterOne = new Chapter ("variables", C1pages);
+Chapter chapterTwo = new Chapter ("datatypes", C2pages);
 
 
-Book bog = new Book();
+List<Chapter> chapters = new List<Chapter>();
+chapters.Add(chapterOne);
 
 
+
+
+//bogen
+Book bog = new Book("Noter", chapters);
 
 bog.Start();
